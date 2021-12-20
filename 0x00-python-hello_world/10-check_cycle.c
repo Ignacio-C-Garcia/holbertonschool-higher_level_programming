a8ea3bc845177b8dc27168f4f6317df4a26eb42f
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-12-20 17:02:21 +0000

    linked list cycle task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+int check_cycle(listint_t *list)
+{
+	listint_t *A2 = list;
+	listint_t *A1 = list;
+
+	while (1)
+	{
+		if (A1->next)
+			A1 = A1->next;
+		if (A2->next)
+		{
+			A2 = A2->next;
+			if (A2->next)
+				A2 = A2->next;
+			else
+				return (0);
+		}
+		else
+			return(0);
+		if (A1 == A2)
+			return (1);
+	}
+	return (0);
+}
