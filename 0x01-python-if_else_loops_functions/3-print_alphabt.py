84c149fd6eefe9945e1259a511c44d142715b3e0
ignacio <ignacioacgarcia@gmail.com>
2021-12-21 12:30:26 -0300

    checker issues

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')):
-    if letter is not ord('q') and letter is not ord('e'):
-        print(chr(letter), end='')
+for letter in range(97, 123):
+    if letter != 113 and letter != 101:
+        print('%c'%(letter), end='')
