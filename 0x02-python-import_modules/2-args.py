74f852d4bc53e79b94ea300b5e4f5d3bf0b5ed64
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-12-23 08:57:56 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    for item in sys.argv:
-        print(item)
+    from sys import argv
+
+    argc = len(argv)
+    index = 0
+
+    if (argc == 1):
+        print('0 arguments.')
+    elif (argc == 2):
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(argc - 1))
+
+    for item in argv[1:]:
+        index = index + 1
+        print('{}: {}'.format(index, item))
