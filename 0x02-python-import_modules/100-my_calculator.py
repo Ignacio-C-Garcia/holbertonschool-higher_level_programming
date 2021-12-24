b967c4e7fbf751b29e4950384b0e2f3f5fe5f40f
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-12-23 16:17:01 -0800

    exit en vez de return

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv
+    from sys import argv, exit
 
     def calculator():
         if len(argv) != 4:
             print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-            return 1
+            exit(1)
 
         a = int(argv[1])
         b = int(argv[3])
@@ -23,6 +23,6 @@ if __name__ == "__main__":
             print('{} / {} = {}'.format(a, b, div(a, b)))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
-            return 1
-        return 0
+            exit(1)
+        exit(0)
     calculator()
