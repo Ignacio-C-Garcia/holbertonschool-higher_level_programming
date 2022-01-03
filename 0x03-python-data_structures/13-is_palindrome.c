f54faecff2102bd2522937d19d313939d53d7087
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-03 12:12:29 -0800

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stddef.h>
+int is_palindrome(listint_t **head)
+{
+	int buffer[1024];
+	listint_t *aux = *head;
+	int counter = 0;
+	int i = 0;
+
+	while(aux)
+	{
+		buffer[counter] = aux->n;
+		counter++;
+		aux = aux->next;
+	}
+
+	for(i = 0; i < counter - 1; i++)
+	{
+		if(buffer[i] != buffer[counter - 1])
+			return (0);
+		counter--;
+	}
+	return (1);
+}
