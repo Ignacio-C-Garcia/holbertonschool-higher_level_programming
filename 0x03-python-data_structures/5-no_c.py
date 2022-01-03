984c1dd76a7907ba71558edd96ae28feacdc4d88
ignacio <ignacioacgarcia@gmail.com>
2022-01-03 12:04:50 -0300

    tasks from 0 until 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    copy_str = ""
+    for letter in my_string:
+        if letter != 'c' and letter != 'C':
+            copy_str = copy_str + letter
+    return copy_str
