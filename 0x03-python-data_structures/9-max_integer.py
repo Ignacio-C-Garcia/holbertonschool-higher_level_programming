681eca5d1c7f1070d46d8553839cfdfa8feea8bb
ignacio <ignacioacgarcia@gmail.com>
2022-01-03 13:40:27 -0300

    task 8 adn 9 dos2unix

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_number = 0
+
+    if my_list == []:
+        return None
+
+    for number in my_list:
+        if number > max_number:
+            max_number = number
+    return max_number
