b427eceae282068f79c186fe0126d0a8fd650452
ignacio <ignacioacgarcia@gmail.com>
2022-01-03 12:45:56 -0300

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        for number in list:
+            print("{:d}".format(number), end='')
+            if number != list[-1]:
+                print(' ', end='')
+        print("")
