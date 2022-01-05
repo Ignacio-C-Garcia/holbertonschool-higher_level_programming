ebc9ff173186ecdf6704eaf7cf12111469ec876f
ignacio <ignacioacgarcia@gmail.com>
2022-01-04 21:41:12 -0300

    task 8 try/except

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key:
+    try:
         a_dictionary.pop(key)
-    return a_dictionary
+        return a_dictionary
+    except:
+        return a_dictionary
