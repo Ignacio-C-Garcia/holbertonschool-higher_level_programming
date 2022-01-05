7345c11082bff6b2312775372a657a9190b39126
ignacio <ignacioacgarcia@gmail.com>
2022-01-04 22:20:55 -0300

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,12 +3,12 @@ def best_score(a_dictionary):
     max_score = 0
     max_key = ""
 
-    try:
-        for key, value in a_dictionary.items():
-            if value > max_score:
-                max_score = value
-                max_key = key
-        return (max_key)
-    finally:
-        return None
-  
+    if not a_dictionary:
+        return (None)
+
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            max_key = key
+
+    return (max_key)
