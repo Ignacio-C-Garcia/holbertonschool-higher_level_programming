9adc91f3df1a75ccb3fa18a5c9abdcc80b8cb84b
ignacio <ignacioacgarcia@gmail.com>
2022-01-04 22:02:48 -0300

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return dict(map(lambda x, y: y * 2, a_dictionary))
+    for key, value in a_dictionary.items():
+        a_dictionary.update({key: value * 2})
+    return a_dictionary
