26086f9beefe9e5edc7a72a66d41a4cc4bedfd98
ignacio <ignacioacgarcia@gmail.com>
2022-01-06 17:27:56 -0300

    pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if type(roman_string) is str and roman_string:
         result = 0
-        numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        numbers =\
+            {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         last_digit = numbers[roman_string[0]]
         for index in roman_string:
             if numbers[index] <= last_digit:
