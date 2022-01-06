20d778d005fcceecd61bd453eac3e9576e493c6c
ignacio <ignacioacgarcia@gmail.com>
2022-01-06 16:58:22 -0300

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is str and roman_string:
+        result = 0
+        numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        last_digit = numbers[roman_string[0]]
+        for index in roman_string:
+            if numbers[index] <= last_digit:
+                result += numbers[index]
+            else:
+                result -= numbers[index]
+            last_digit = numbers[index]
+        return abs(result)
+    return 0
