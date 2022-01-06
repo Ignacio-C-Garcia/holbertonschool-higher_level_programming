f9ef258efe57ffc1cd34cc8df7bc443560fe0cb9
ignacio <ignacioacgarcia@gmail.com>
2022-01-06 18:12:23 -0300

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     b_dictionary = a_dictionary.copy()
 
-    for key, number in a_dictionary.items():
+    for key, number in b_dictionary.items():
         if number == value:
-            b_dictionary.pop(key)
-    return (b_dictionary)
+            a_dictionary.pop(key)
+    return (a_dictionary)
