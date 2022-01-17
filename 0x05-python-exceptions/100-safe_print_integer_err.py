c434041b42ecf9417e1722d8454844bbc25572cb
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-17 13:47:44 -0300

    .

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
-    except (ValueError, TypeError) as ex:
+    except Exception as ex:
         sys.stderr.write('Exception: {}'.format(ex))
         return False
 
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as ex:
+        sys.stderr.write(f'Exception: {ex}')
+        return None
