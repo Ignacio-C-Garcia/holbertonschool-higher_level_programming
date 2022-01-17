2655ced5263d9f5e10c4c1c37344db34ff62a4c0
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-17 07:35:50 -0800

    forgot to use finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,4 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
     except IndexError:
         print('out of range')
         list_results.append(0)
-    return list_results
+    finally:
+        return list_results
