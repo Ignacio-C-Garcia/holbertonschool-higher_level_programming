af5a23a2a0adee0cf089f3977241b6cd8efe5b08
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-31 18:50:21 -0300

    chuchu

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,19 +7,3 @@ def add_attribute(obj, name, value):
     if obj.__dict__ == 0:
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
-
-
-class MyClass():
-    """adentro"""
-    pass
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
