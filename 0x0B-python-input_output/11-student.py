ab0214afc5d5c13589d4d0da16447ba92a3b933d
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-02-01 13:33:41 -0800

    task 111

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""name"""
+
+
+class Student:
+    """name"""
+    def __init__(self, first_name, last_name, age):
+        """name"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """name"""
+        aux = {}
+        if type(attrs) == list:
+            for item in attrs:
+                if type(item) != str:
+                    return self.__dict__
+            for item in set(self.__dict__.keys()) & set(attrs):
+                aux.setdefault(item, self.__dict__.get(item))
+            return aux
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """comment"""
+        aux = self.to_json(json.keys())
+        for item in aux:
+            self.__dict__[item] = json[item]
