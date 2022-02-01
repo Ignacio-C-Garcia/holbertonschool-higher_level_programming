4573fa9d4c4fd3bc0613d90f6bbe1139e8161e32
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-02-01 12:11:03 -0800

    pep8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""name"""
+
+
 def append_write(filename="", text=""):
+    """name"""
     with open(filename, mode='a', encoding='utf-8') as potato:
         return potato.write(text)
