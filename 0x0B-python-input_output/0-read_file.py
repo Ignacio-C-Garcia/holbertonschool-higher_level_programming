eb515b1e4c7ab53cb5a5b399016d6a229aa68b1b
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-02-01 12:09:43 -0800

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """name"""
     with open(filename, mode='r', encoding='utf-8') as text:
         for line in text:
-            print(line,end='')
+            print(line, end='')
