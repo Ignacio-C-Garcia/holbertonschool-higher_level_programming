9f8e86896bce55f00302b04535097d3a87307704
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-03-09 15:14:54 -0800

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_genres.name AS genre, count(tv_show_genres.genre_id) AS number_of_shows FROM tv_show_genres 
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
+
