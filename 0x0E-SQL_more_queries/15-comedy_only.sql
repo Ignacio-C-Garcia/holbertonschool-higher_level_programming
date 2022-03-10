21b08311f15e2b1723e5960eec043e2bffedc8cd
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-03-09 17:31:18 -0800

    ordenado por title

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,5 +4,5 @@ JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name="COMEDY"
-ORDER BY name ASC;
+ORDER BY title ASC;
 
