a898b428a7aa757785769e71ff2f4459bf9afee0
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-03-09 17:29:25 -0800

    task 15 UWU

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title AS title FROM tv_shows 
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name="COMEDY"
+ORDER BY name ASC;
+
