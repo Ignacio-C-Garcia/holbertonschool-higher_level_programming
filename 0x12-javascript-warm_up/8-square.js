ca3a85662e40756aab3ef0077246d5b522ced31f
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 12:46:49 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let size = parseInt(process.argv[2]);
+if (isNaN(size))
+	console.log('Missing size');
+let size2 = size
+while (size2)
+{	console.log('X'.repeat(size));
+	size2--;
+}
