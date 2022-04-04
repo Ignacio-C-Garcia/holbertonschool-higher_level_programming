31f6e7b75c9e81c1946a6f8e555e70f346325b98
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 12:28:22 -0700

    fixing output error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 let repeat = parseInt(process.argv[2]);
-if (isNaN(repeat)) { console.log('Missing number of occurrences'); }
-while (repeat) {
-  console.log('C is fun');
-  repeat--;
+if (isNaN(repeat)) { console.log('Missing number of occurrences'); } else {
+  while (repeat) {
+    console.log('C is fun');
+    repeat--;
+  }
 }
