028d3f485b69f1501c5aed19610e162d1dc7224e
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 12:18:41 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
+while (array[index] !== undefined) {
+  console.log(array[index]);
+
+  index++;
+}
