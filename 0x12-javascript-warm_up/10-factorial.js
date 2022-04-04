57e6b773848ee164e1da6382b5dd41019b445cf8
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 13:11:42 -0700

    semicodestyle fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 let number = parseInt(process.argv[2]);
-if (isNaN(number))
-	number = 1;
-function factorial(number)
-{
-	result = 0;
-	if (number === 0)
-		return 1;
-	result = number * factorial(number - 1);
-	return result;
+if (isNaN(number)) { number = 1; }
+function factorial (number) {
+  let result = 0;
+  if (number === 0) { return 1; }
+  result = number * factorial(number - 1);
+  return result;
 }
 console.log(factorial(number));
