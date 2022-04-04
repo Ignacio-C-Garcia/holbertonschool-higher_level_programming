5449224635c5d5e2caeecd319a700d5ea9ba11e1
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 13:07:11 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let number = parseInt(process.argv[2]);
+if (isNaN(number))
+	number = 1;
+function factorial(number)
+{
+	result = 0;
+	if (number === 1)
+		return 1;
+	result = number + factorial(number - 1);
+	return result;
+}
+console.log(factorial(number));
