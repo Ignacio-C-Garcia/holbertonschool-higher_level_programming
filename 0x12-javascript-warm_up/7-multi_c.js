4d73f592897ebb45f5f6c83667631153bec4983c
Ignacio <ignacioacgarcia@gmail.com>
2022-04-04 12:31:55 -0700

    border case fixed:negative numbers

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let repeat = parseInt(process.argv[2]);
 if (isNaN(repeat)) { console.log('Missing number of occurrences'); } else {
-  while (repeat) {
+  while (repeat > 0) {
     console.log('C is fun');
     repeat--;
   }
