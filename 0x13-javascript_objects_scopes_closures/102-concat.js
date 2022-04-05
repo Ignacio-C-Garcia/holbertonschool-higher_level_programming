83a91935e799a1dc21b6c9bb9e98423fdf1b8571
Ignacio <ignacioacgarcia@gmail.com>
2022-04-05 15:02:39 -0700

    task with semicodestyle

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
-fs = require('fs');
+const fs = require('fs');
 const buff = Buffer.alloc(1024);
 const buff2 = Buffer.alloc(1024);
 fs.open(process.argv[2], 'r', (err, fd) => {
+  if (err) { console.log(err); }
   fs.read(fd, buff, 0, 1024, 0, (err, bytes, buff) => {
+    if (err) { console.log(err); }
     fs.open(process.argv[3], 'r', (err, fd2) => {
+      if (err) { console.log(err); }
       fs.read(fd2, buff2, 0, 1024, 0, (err, bytes, buff2) => {
+        if (err) { console.log(err); }
         fs.open(process.argv[4], 'a', (err, fd3) => {
+          if (err) { console.log(err); }
           fs.write(fd3, buff, 0, 1024, 0, (err, bytes, buffer) => {
+            if (err) { console.log(err); }
             fs.write(fd3, buff2, 0, 1024, 0, (err, bytes, buffer) => {
-
+              if (err) { console.log(err); }
             });
           });
         });
       });
     });
-	  });
-	  });
+  });
+});
