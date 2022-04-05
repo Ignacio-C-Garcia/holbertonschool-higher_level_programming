d274a2cd55d8ea2143d1722b73e80a7ebc5ec3eb
Ignacio <ignacioacgarcia@gmail.com>
2022-04-05 15:18:35 -0700

    sadness

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
-const buff = Buffer.alloc(10);
-const buff2 = Buffer.alloc(18);
+const buff = Buffer.alloc(1024);
+const buff2 = Buffer.alloc(1024);
 fs.open(process.argv[2], 'r', (err, fd) => {
   if (err) { console.log(err); }
-  fs.read(fd, buff, 0, 10, 0, (err, bytes, buff) => {
+  fs.read(fd, buff, 0, 1024, 0, (err, bytes1, buff) => {
     if (err) { console.log(err); }
     fs.open(process.argv[3], 'r', (err, fd2) => {
       if (err) { console.log(err); }
-      fs.read(fd2, buff2, 0, 18, 0, (err, bytes, buff2) => {
+      fs.read(fd2, buff2, 0, 1024, 0, (err, bytes2, buff2) => {
         if (err) { console.log(err); }
         fs.open(process.argv[4], 'a', (err, fd3) => {
           if (err) { console.log(err); }
-          fs.write(fd3, buff, 0, 10, 0, (err, bytes, buffer) => {
+          fs.write(fd3, buff, 0, bytes1, 0, (err, bytes, buffer) => {
             if (err) { console.log(err); }
-            fs.write(fd3, buff2, 0, 18, 0, (err, bytes, buffer) => {
+            fs.write(fd3, buff2, 0, bytes2, 0, (err, bytes, buffer) => {
               if (err) { console.log(err); }
             });
           });
