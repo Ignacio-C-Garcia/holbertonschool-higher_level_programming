56e1feebc48a4e60afc1e0483eadf59583470e8e
Ignacio <ignacioacgarcia@gmail.com>
2022-04-05 15:38:15 -0700

    task hell

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,17 +4,17 @@ const buff = Buffer.alloc(1024);
 const buff2 = Buffer.alloc(1024);
 if (process.argv[4] !== undefined) {
   fs.open(process.argv[2], 'r', (err, fd) => {
-    if (err) { console.log(err); }
+    if (err) { console.log(err); return; }
     fs.read(fd, buff, 0, 1024, 0, (err, bytes1, buff) => {
-      if (err) { console.log(err); }
+      if (err) { console.log(err); return; }
       fs.open(process.argv[3], 'r', (err, fd2) => {
         if (err) { console.log(err); }
         fs.read(fd2, buff2, 0, 1024, 0, (err, bytes2, buff2) => {
-          if (err) { console.log(err); }
+          if (err) { console.log(err); return; }
           fs.open(process.argv[4], 'a', (err, fd3) => {
-            if (err) { console.log(err); }
+            if (err) { console.log(err); return; }
             fs.write(fd3, buff, 0, bytes1, 0, (err, bytes, buffer) => {
-              if (err) { console.log(err); }
+              if (err) { console.log(err); return; }
               fs.write(fd3, buff2, 0, bytes2, 0, (err, bytes, buffer) => {
                 if (err) { console.log(err); }
               });
