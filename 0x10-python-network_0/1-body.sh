c4a2a97e2c7c2166d18aa8a3cf146ab4a042dbd7
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-22 19:07:38 +0000

    test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #comment
-if [ $(curl -o -I -L -s -w "%{http_code}" "$1") -eq 200 ]; then curl -L "$1"; fi
+if [ $(curl -o -I -L -s -w "%{http_code}" "$1") -eq 200 ]; then curl -L -X GET "$1"; fi
