e9f738d9b73ac11788a131df7766e4b87beb9347
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 21:33:01 +0000

    pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,12 +5,15 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    response = requests.get(
-            'https://api.github.com/user', auth=(argv[1], argv[2]))
-    if response.status_code >= 400:
-        print('None')
-    else:
-        try:
-            print(response.json()['id'])
-        except Exception as f:
+    try:
+        response = requests.get(
+                'https://api.github.com/user', auth=(argv[1], argv[2]))
+        if response.status_code >= 400:
             print('None')
+        else:
+            try:
+                print(response.json()['id'])
+            except Exception as f:
+                print('None')
+    except Exception as f:
+        print("")
