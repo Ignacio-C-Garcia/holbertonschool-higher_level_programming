546d4147bbec6544caa880bf97d7be16f24fd7a3
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 10:36:54 -0700

    not execute when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """comment"""
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    print('Body response:')
-    body = response.read()
-    print('\t- type:', type(body))
-    print('\t- content:', body)
-    print('\t- utf8 content', body.decode(encoding='UTF-8'))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        print('Body response:')
+        body = response.read()
+        print('\t- type:', type(body))
+        print('\t- content:', body)
+        print('\t- utf8 content', body.decode(encoding='UTF-8'))
