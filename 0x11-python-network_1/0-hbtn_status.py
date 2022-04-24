b05dd5b702cc5da3df8c44f49a2ebbf734c1bf17
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 10:53:18 -0700

    comments?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""comment"""
+"""comment of the module"""
 import urllib.request
+
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         print('Body response:')
