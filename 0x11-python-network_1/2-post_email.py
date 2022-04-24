27dcd08312059c23d1a8cfea1409beceeb5952a6
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 11:37:17 -0700

    testing

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""comment of the module"""
+from urllib import request
+from urllib import parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = parse.urlencode({'email': argv[2]}).encode('ascii')
+    req =  request.Request(argv[1], data=data)
+    with request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
