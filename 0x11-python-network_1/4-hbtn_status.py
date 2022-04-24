4c8b51f1df6cd737c6da54fb78c09d0012e74d96
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 19:17:43 +0000

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""comment of the module"""
+import requests
+
+
+if __name__ == "__main__":
+	response = requests.get('https://intranet.hbtn.io/status')
+	print('\t- type:', type(response.text))
+	print('\t- content:', response.text)
