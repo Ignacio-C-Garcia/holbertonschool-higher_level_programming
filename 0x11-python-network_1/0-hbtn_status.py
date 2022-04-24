8a04b8e7efa4f2b85cb21c34bcc1ec2c4166f40a
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 10:32:45 -0700

    typo fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     print('Body response:')
     body = response.read()
-    print('\t', '- type: ', type(body))
-    print('\t', '- content: ', body)
-    print('\t', '- utf8 content ', body.decode(encoding='UTF-8'))
+    print('\t- type:', type(body))
+    print('\t- content:', body)
+    print('\t- utf8 content', body.decode(encoding='UTF-8'))
