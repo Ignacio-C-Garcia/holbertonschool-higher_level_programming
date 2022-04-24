f9501f8006555787737dc429510989c2df766b6d
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 19:20:51 +0000

    missing one line

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,6 @@ import requests
 
 if __name__ == "__main__":
 	response = requests.get('https://intranet.hbtn.io/status')
+	print('Body response:')
 	print('\t- type:', type(response.text))
 	print('\t- content:', response.text)
