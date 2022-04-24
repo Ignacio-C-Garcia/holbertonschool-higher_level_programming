b871b583fc7df2f907aa376368d4e081e391a6c3
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 19:03:20 +0000

    without pep8

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""comment of the module"""
+from urllib import request
+from urllib import parse
+from sys import argv
+from urllib import error
+
+if __name__ == "__main__":
+	try:
+		with request.urlopen(argv[1]) as response:
+        		body = response.read().decode('utf-8')
+        		print(body)
+	except error.HTTPError as Fail:
+		print('Error code:', Fail.code)
