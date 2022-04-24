3ace488a59ffb9acea3de2e09a5a86c60ea01f69
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 21:28:10 +0000

    task 11

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
         response = requests.get(
-'https://api.github.com/user', auth=(argv[1], argv[2]))
+   'https://api.github.com/user', auth=(argv[1], argv[2]))
         if response.status_code >= 400:
             print('None')
         else:
---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""comment of the module"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    response = requests.get(
+   'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]), headers={"Accept": "application/vnd.github.v3+json"})
+    response = response.json()
+    for x in range(10):
+        print('{}: {}'.format(response[x]['sha'], response[x]['commit']['author']['name']))
