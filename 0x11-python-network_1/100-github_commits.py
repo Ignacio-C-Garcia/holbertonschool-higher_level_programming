ca4ce880377a2446c623448f4d4aaef7cd04087b
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 22:02:55 +0000

    border cases

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,15 @@ from sys import argv
 if __name__ == "__main__" and len(argv) == 3:
     response = requests.get(
    'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]), headers={"Accept": "application/vnd.github.v3+json"})
-    response = response.json()
-    for x in range(10):
-        print('{}: {}'.format(response[x]['sha'], response[x]['commit']['author']['name']))
+    if response.status_code >= 400:
+        print('Error code:', response.status_code)
+    else:
+        try:
+            response = response.json()
+        except Exception as f:
+            print('None')
+        rep = 10
+        if len(response) < 10:
+            rep = len(response)
+        for x in range(rep):
+            print('{}: {}'.format(response[x]['sha'], response[x]['commit']['author']['name']))
