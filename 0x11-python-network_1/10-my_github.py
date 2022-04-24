a06c14bc57f50ad038a9941cc9e3d84a5700f7fd
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-24 20:26:53 +0000

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""comment of the module"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+        response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        if response.status_code >= 400:
+            print('Error code:', response.status_code)
+        else:
+            print(response.json()['id'])
