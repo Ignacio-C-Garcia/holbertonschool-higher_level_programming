08955c54049a39c1d4ced192ef8fa73bdc8e59e4
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-17 23:31:30 +0000

    semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,15 +3,13 @@ const argv = require('process').argv;
 const axios = require('axios').default;
 // Make a reques
 const dict = {};
-  axios.get(argv[2]).then(function (response2) {
-    for (const index in response2.data) {
-      if (response2.data[index].completed) {
-	if (response2.data[index].userId in dict)
-        	dict[response2.data[index].userId] = dict[response2.data[index].userId] + 1;
-	else {
-	dict[response2.data[index].userId] = 1;
-}
+axios.get(argv[2]).then(function (response2) {
+  for (const index in response2.data) {
+    if (response2.data[index].completed) {
+      if (response2.data[index].userId in dict) { dict[response2.data[index].userId] = dict[response2.data[index].userId] + 1; } else {
+        dict[response2.data[index].userId] = 1;
       }
     }
-    console.log(dict);
-  });
+  }
+  console.log(dict);
+});
