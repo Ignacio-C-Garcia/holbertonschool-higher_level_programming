eb6a455bcb5dfb8571fce163e7bc2e004f7ab059
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-17 15:41:36 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,12 @@
 const argv = require('process').argv;
 const axios = require('axios').default;
 
-// Make a request for a user with a given ID
+// Make a reques
 axios.get(argv[2])
   .then(function (response) {
     // handle success
     console.log('code:', response.status);
+  }).catch(function (error) {
+    // handle error
+    console.log(error.response);
   })
