35767a0bbf12ca95ca9aaa78d2e4398e8a085195
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-17 17:50:01 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,14 +4,12 @@ const axios = require('axios').default;
 const fs = require('fs');
 // Make a reques
 axios.get(argv[2]).then(function (response) {
-  
-fs.writeFile(argv[3], response.data, { encoding: 'utf8' }, (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-});
-
-}).catch(function (error) {
-    // handle error
-    console.log(error);
+  fs.writeFile(argv[3], response.data, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.error(err);
+    }
   });
+}).catch(function (error) {
+  // handle error
+  console.log(error);
+});
