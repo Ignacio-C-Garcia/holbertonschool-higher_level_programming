5f0736109050b6a6fbe9991b3297897f9b8df96a
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-17 23:29:54 +0000

    task 6 again

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,14 +3,15 @@ const argv = require('process').argv;
 const axios = require('axios').default;
 // Make a reques
 const dict = {};
-axios.get('https://jsonplaceholder.typicode.com/users').then(function (response) {
-  for (let size = 1; size <= response.data.length; size++) { dict[`${size}`] = 0; }
   axios.get(argv[2]).then(function (response2) {
     for (const index in response2.data) {
       if (response2.data[index].completed) {
-        dict[response2.data[index].userId] = dict[response2.data[index].userId] + 1;
+	if (response2.data[index].userId in dict)
+        	dict[response2.data[index].userId] = dict[response2.data[index].userId] + 1;
+	else {
+	dict[response2.data[index].userId] = 1;
+}
       }
     }
     console.log(dict);
   });
-});
