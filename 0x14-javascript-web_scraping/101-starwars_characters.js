9307aedced6fb7922dd359f7eafc332a6fa36870
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-18 01:09:23 +0000

    politicamente correcto

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,14 +2,13 @@
 const argv = require('process').argv;
 const axios = require('axios').default;
 // Make a reques
-function printer()
-{
-axios.get(`https://swapi-api.hbtn.io/api/films/${argv[2]}/`).then(function (response) { response.data.characters.forEach(myFunction) });
+async function getlist () {
+  const response = await axios.get(`https://swapi-api.hbtn.io/api/films/${argv[2]}/`);
+
+  for (const person in response.data.characters) {
+    await axios.get(response.data.characters[person]).then(function (response2) {
+      console.log(response2.data.name);
+    }).catch(error => { console.log(error); });
+  }
 }
-async function myFunction (url) {
-await  new Promise(resolve => {
-axios.get(url).then(function (response) {
-    console.log(response.data.name);
-  });
-})}
-printer();
+getlist();
