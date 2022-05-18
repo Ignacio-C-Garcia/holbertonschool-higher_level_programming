eeab259896e90cbd5f6372c7de37e8f57845ec1e
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-18 00:32:12 +0000

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,14 +2,14 @@
 const argv = require('process').argv;
 const axios = require('axios').default;
 // Make a reques
-axios.get(`https://swapi-api.hbtn.io/api/films/${argv[2]}/`).then(function (response) {
-  response.data.characters.forEach(myFunction);
-}).catch(function (error) {
-  // handle error
-  console.log(error);
-});
+function printer()
+{
+axios.get(`https://swapi-api.hbtn.io/api/films/${argv[2]}/`).then(function (response) { response.data.characters.forEach(myFunction) });
+}
 async function myFunction (url) {
-  await axios.get(url).then(function (response) {
+await  new Promise(resolve => {
+axios.get(url).then(function (response) {
     console.log(response.data.name);
   });
-}
+})}
+printer();
