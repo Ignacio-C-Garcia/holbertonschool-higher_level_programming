31d272a69dbb1b3f9df35ce50b10049d28e4408c
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-18 01:33:53 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,21 @@
 const argv = require('process').argv;
 const axios = require('axios').default;
 let counter = 0;
-const person = 'https://swapi-api.hbtn.io/api/people/18/';
+const person = '/18/';
 // Make a reques
 axios.get(argv[2])
   .then(function (response) {
     for (const film in response.data.results) {
-      if (response.data.results[film].characters.includes(person)) {
-        counter = counter + 1;
-      }
+	for (const line in response.data.results[film].characters)
+	{
+      		if (response.data.results[film].characters[line].includes(person)) {
+        	counter = counter + 1;
+	      	}
+	}
     }
     console.log(counter);
   }).catch(function (error) {
     // handle error
     console.log(error);
   });
+
